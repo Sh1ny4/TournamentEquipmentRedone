66ab0970e1dbeaa029146a63eadab7034c70405e
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-09-17 19:15:40 +0200

    Initial commit

---
+++ TournamentEquipmentRedone/TournamentEquipmentRedone/Submodule.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace TournamentEquipmentRedone
+{
+    public class Submodule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("wipo.patches").PatchAll();
+        }
+    }
+}
---
+++ TournamentEquipmentRedone/TournamentEquipmentRedone/TournamentEquipmentRedone/GetParticipantArmourPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+
+namespace TournamentEquipmentRedone.TournamentEquipmentRedone
+{
+    [HarmonyPatch(typeof(DefaultTournamentModel), nameof(DefaultTournamentModel.GetParticipantArmor))]
+    internal class GetParticipantArmourPatch
+    {
+        [HarmonyPostfix]
+        static void Postfix(ref Equipment __result, CharacterObject participant)
+        {
+            if (CampaignMission.Current.Mode == MissionMode.Tournament)
+            {
+                string text = string.Concat(new object[] { "tournament_", Settlement.CurrentSettlement.Culture.StringId });
+                __result = (Game.Current.ObjectManager.GetObject<CharacterObject>(text) ?? Game.Current.ObjectManager.GetObject<CharacterObject>("gear_practice_dummy_empire")).RandomBattleEquipment;
+            }
+            // The weapon loadout still is changed by the "tournament_template_<culture>_<amount>_participant_set_vX" NPC, but now the armour can be changed by having an NPC with the ID "tournament_<culture>"
+        }
+    }
+}
